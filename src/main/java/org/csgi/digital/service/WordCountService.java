7c1e3c5cc03f368ce0eae0606fd34676d39e194c
Ankit Agarwal <ankit22.a@tcs.com>
2022-10-13 19:54:02 +0200

    Apply rules to list of words to get count & filtered words

---
+++ src/main/java/org/csgi/digital/service/WordCountService.java
@@ -0,0 +1,45 @@
+package org.csgi.digital.service;
+
+import org.csgi.digital.rule.CharacterCountRule;
+import org.csgi.digital.rule.PrefixRule;
+
+import java.util.List;
+
+/**
+ *
+ * @author Ankit Agarwal
+ */
+public class WordCountService {
+
+    /**
+    * This will return the count of the words based on the length of the word
+    * @param words  input as list of word
+    * @param length length of characters longer than specified value
+    */
+    public long findWordsGreaterThanCertainLength(List<String> words, int length) {
+        var characterCountRule = new CharacterCountRule(length, null);
+        return characterCountRule.onNext(words.stream()).count();
+    }
+
+    /**
+     * This will return the list of words based on the prefix rule
+     * @param words  input as list of word
+     * @param prefix prefix should be matched with the word prefix
+     */
+    public List<String> findWordsWithMatchingPrefix(List<String> words, String prefix) {
+        var prefixIgnoreCaseMatchRule = new PrefixRule(prefix, null);
+        return prefixIgnoreCaseMatchRule.onNext(words.stream()).toList();
+    }
+
+    /**
+     * This will return the list of words based on the prefix rule as well as length rule
+     * @param words  input as list of word
+     * @param prefix prefix should be matched with the word prefix
+     * @param length length of characters longer than specified value
+     */
+    public List<String> findWordsGreaterThanCertainLengthAndMatchingPrefix(List<String> words, int length, String prefix) {
+        var characterCountRule = new CharacterCountRule(length, null);
+        var prefixIgnoreCaseMatchRule = new PrefixRule(prefix, characterCountRule);
+        return prefixIgnoreCaseMatchRule.onNext(words.stream()).toList();
+    }
+}
